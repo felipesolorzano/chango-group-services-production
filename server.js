1b9a16c26bff42264d73547deb3f11163b898228
felipe solorzano <felipe.de.jesus.solorzano@gmail.com>
2023-12-11 21:26:46 -0600

    up

--- server.js
+++ server.js
@@ -4,8 +4,11 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 };
 Object.defineProperty(exports, "__esModule", { value: true });
 require("reflect-metadata");
-const source_map_support_1 = __importDefault(require("source-map-support"));
-const standalone_1 = require("@adonisjs/core/build/standalone");
-source_map_support_1.default.install({ handleUncaughtExceptions: false });
-new standalone_1.Ignitor(__dirname).httpServer().start();
-//# sourceMappingURL=server.js.map
+
+exports.handler = async (event) => {
+    const response = {
+      statusCode: 200,
+      body: JSON.stringify("Hello from Lambda and Github!"),
+    }
+    return response
+  }
