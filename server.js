6d8763eb5073e8c9b348664b8de3b9b9f0c0b04f
felipe solorzano <felipe.de.jesus.solorzano@gmail.com>
2023-12-11 21:33:41 -0600

    up4

--- server.js
+++ server.js
@@ -8,12 +8,3 @@ const source_map_support_1 = __importDefault(require("source-map-support"));
 const standalone_1 = require("@adonisjs/core/build/standalone");
 source_map_support_1.default.install({ handleUncaughtExceptions: false });
 new standalone_1.Ignitor(__dirname).httpServer().start();
-
-
-exports.handler = async (event) => {
-    const response = {
-      statusCode: 200,
-      body: JSON.stringify("3 Hello from Lambda and Github!"),
-    }
-    return response
-  }
