ece7f8a94102aff38e0e4dadc919f1a24cd0f450
felipe solorzano <felipe.de.jesus.solorzano@gmail.com>
2023-12-11 20:45:26 -0600

    first commit

---
+++ providers/AppProvider.js
@@ -0,0 +1,18 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+class AppProvider {
+    app;
+    constructor(app) {
+        this.app = app;
+    }
+    register() {
+    }
+    async boot() {
+    }
+    async ready() {
+    }
+    async shutdown() {
+    }
+}
+exports.default = AppProvider;
+//# sourceMappingURL=AppProvider.js.map
